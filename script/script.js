166daeb028a153fc93841dd7a14021c749a9aa80
Devhocke <71835476+DevHocke@users.noreply.github.com>
2021-08-29 23:29:31 +0200

    Some text and image changes.

--- script/script.js
+++ script/script.js
@@ -19,3 +19,28 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function weatherApp( cityID ) {
+  let key = '0d9ea726f6664ca8166acb9c4cf0dbcf';
+  fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+  .then(function(resp) { return resp.json() }) // Convert data to json
+  .then(function(data) {
+    drawWeather(data);
+  })
+  .catch(function() {
+    // catch any errors
+  });
+}
+
+window.onload = function() {
+  weatherApp( 2700199 );
+}
+
+function drawWeather( d ) {
+var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+
+document.getElementById('description').innerHTML = d.weather[0].description;
+document.getElementById('temp').innerHTML = celcius + '&deg;';
+document.getElementById('location').innerHTML = d.name;
+}
